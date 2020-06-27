cded3476d3868ce685a543caab5879c272c5c462
kkowalRepository <67493641+kkowalRepository@users.noreply.github.com>
2020-06-27 06:02:27 +0200

    tempCheck version 1

---
+++ Xcode playground/tempCheck.playground/Contents.swift
@@ -0,0 +1,9 @@
+import UIKit
+// ---------------check for temperature-------------
+
+var temperatureInFahrenheit = 97
+if temperatureInFahrenheit <= 32 {
+    print("It's very cold. Consider wearing a scarf.")
+} else if temperatureInFahrenheit >= 86 {
+    print("It's really warm. Don't forget to wear sunscreen.")
+}
