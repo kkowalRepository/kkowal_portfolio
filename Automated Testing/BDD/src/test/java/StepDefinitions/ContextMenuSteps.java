41c5c6950487c63d0a6e1c7ac1b3a28cdcf420d1
kkowalRepository <67493641+kkowalRepository@users.noreply.github.com>
2020-07-03 12:12:24 +0200

    contextMenu project

--- Automated Testing/BDD/src/test/java/StepDefinitions/ContextMenuSteps.java
+++ Automated Testing/BDD/src/test/java/StepDefinitions/ContextMenuSteps.java
@@ -4,15 +4,11 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-
-import java.awt.event.MouseEvent;
-import java.util.concurrent.SynchronousQueue;
+import org.openqa.selenium.interactions.Actions;
 
 public class ContextMenuSteps {
 
@@ -25,29 +21,29 @@ public class ContextMenuSteps {
         driver = new ChromeDriver();
         driver.manage().deleteAllCookies();
         driver.get("http://the-internet.herokuapp.com");
-        System.out.println("user is on a website: http://the-internet.herokuapp.com");
     }
 
     @When("^user clicks \"([^\"]*)\" form the list$")
-    public void user_clicks_something_form_the_list(String strArg1)  {
-        WebElement contextBtn = driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[7]/a"));
-        contextBtn.click();
-        System.out.println("user clicked context menu from the list");
+    public void user_clicks_something_form_the_list(String strArg1) {
+        WebElement contextLink = driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[7]/a"));
+        contextLink.click();
     }
 
     @Then("^user can see an alert$")
     public void user_can_see_an_alert() {
+
     }
 
     @And("^user is on \"([^\"]*)\" page$")
-    public void user_is_on_something_page(String strArg1)  {
+    public void user_is_on_something_page(String strArg1) {
     }
 
     @And("^user rightclicks the box to trigger a JavaScript alert$")
     public void user_rightclicks_the_box_to_trigger_a_javascript_alert() {
+//      Right click the box
+        Actions action = new Actions(driver);
         WebElement box = driver.findElement(By.id("hot-spot"));
-        box.Mouse;
-
+        action.contextClick(box).perform();
     }
 
 
